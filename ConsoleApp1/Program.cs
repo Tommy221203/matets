0ffcc1f201e346f45482979d64e6a2ee86463ad3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:28:15 +0200

    parametri numeri

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,17 @@ namespace ConsoleApp1
             Console.WriteLine("inserisci un numero");
             double n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("inserisci il secondo numero");
+            if (n1<5)
+            {
+                Console.WriteLine("ilnumero inserito è minore di 5");
+            }
             double n2 = int.Parse(Console.ReadLine());
+            {
+                Console.WriteLine("il numero inserito è maggiore di 5");
+            }
             double somma = n1 + n2;
             Console.WriteLine($"il risultato è {somma}");
+
         }
     }
 }
