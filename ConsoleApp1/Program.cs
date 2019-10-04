af0e6a6dcf8557c3ad05962e20b9bcfdee60a374
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:24:12 +0200

    somma 2 numeri

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ciao");
+            Console.WriteLine("inserisci un numero");
+            double n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il secondo numero");
+            double n2 = int.Parse(Console.ReadLine());
+            double somma = n1 + n2;
+            Console.WriteLine($"il risultato è {somma}");
         }
     }
 }
